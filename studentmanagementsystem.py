59b7fc8008416b878c577e47cb46fc563b261f3c
Erick Maina <114502636+erickSteward@users.noreply.github.com>
2024-08-30 20:33:14 +0300

    Week 2

--- studentmanagementsystem.py
+++ studentmanagementsystem.py
@@ -1,28 +1,34 @@
 studentList = []
 
+# Function to acquire and add a student details for a user
 def addStudent():
-    # Prompt user to add student details(name, age, grade)
+    # Loop the process for adding student details
     while True:
+        # Prompt user to add student details(name, age, grade)
         print(' ')
         print('Please add student details as prompted')
         studentName = input('Enter student name: ')
         studentAge = input('Enter student age: ')
         studentGrade = input('Enter student grade: ')
-        if studentName == "" or not studentAge.isdigit or not studentAge.isdigit:
+        # validate the users input of the student details
+        if studentName == "" :
+            if not studentAge.isdigit or not studentGrade.isdigit:
+                print('Age and grade must be numbers.')
             print('Student details cannot be empty')
         
-
-        # Convert the age and the grade to integer
+        # store the student details in a student dictionary
         student = {
             'name': studentName,
             'age' : studentAge,
             'grade' : studentGrade
         }
 
+        # add the student dictionary to the global student list(studentList)
         studentList.append(student)
 
         print(f'Student {studentName} has been added successfully\n')
 
+        # ask the user where to continue adding or stop.
         continuation = input('Do you want to continue adding student(yes/no)')
         if continuation == 'yes':
             print('Ok')
@@ -31,18 +37,19 @@ def addStudent():
             print('Sure')
             break
         
-
+# function to display stored students details for the global studentList
 def displayStudent():
     if not studentList:
         print(" ")
         print("No records found")
         print(" ")
         return
-    
+    # Loop through while displaying each student details form the list.
     for student in studentList:
         print(' ')
         print(f"Student Name: {student['name']}, Age: {student['age']}, Grade:{student['grade']}")
 
+# function to display a menu for the user to choose what the need
 def main():
     while True:
         print('Welcome to Murima School Management System')
@@ -61,7 +68,7 @@ def main():
         else:
             print('Please enter 1, 2, or 3 Kindly.')
 
-
+# launching the program
 main()
 
 
